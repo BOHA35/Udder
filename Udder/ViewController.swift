91bcfeee8bbe20d766bc4c3d638c6321bc1d2b85
Andras Bohacsek <boha@20014c4c2454d300ac43013336e9184c.catv.pool.telekom.hu>
2019-02-21 21:11:17 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Andras Bohacsek on 2019. 02. 21..
+//  Copyright © 2019. Andras Bohacsek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
